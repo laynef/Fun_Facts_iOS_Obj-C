1c2faea8b8fe2ab81d4249bccbd5fe7fe77d371b
Layne Faler <laynefaler@gmail.com>
2016-04-22 20:49:43 -0400

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Layne Faler on 4/22/16.
+//  Copyright © 2016 Mean Green Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FunFacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FunFacts
+//
+//  Created by Layne Faler on 4/22/16.
+//  Copyright © 2016 Mean Green Machine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
